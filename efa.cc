dc2c28d8904a479c2aa3611d81bfd0676f96cb3e
Kelly Norton <kellegous@gmail.com>
2021-06-02 21:17:05 -0400

    .

--- efa.cc
+++ efa.cc
@@ -2,6 +2,7 @@
 #include "pkg/rpc/rpc.grpc.pb.h"
 
 #include <cairomm/cairomm.h>
+#include <chrono>
 #include <grpcpp/grpcpp.h>
 #include <iomanip>
 #include <iostream>
@@ -13,12 +14,21 @@
 
 namespace {
 
+const auto kTau = 2 * M_PI;
+
+void Seed(std::default_random_engine& rng) {
+    auto t = std::chrono::duration_cast<std::chrono::nanoseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+    );
+    rng.seed(t.count());
+}
+
 void CreateCharges(
     std::vector<std::shared_ptr<Charge>>* dst,
+    std::default_random_engine& rng,
     double w,
     double h,
     size_t n) {
-    std::default_random_engine rng;
     std::uniform_real_distribution<double> hdist(0, h);
     std::uniform_real_distribution<double> cdist(-10, 10);
 
@@ -62,6 +72,12 @@ public:
 
     void Set(Cairo::Context& context) {
         context.set_source_rgb(R(), G(), B());
+    }
+
+    void SetWithAlpha(
+        Cairo::Context& context,
+        double alpha) {
+        context.set_source_rgba(R(), G(), B(), alpha);
     }
 private:
     uint32_t rgb_;
@@ -107,8 +123,35 @@ private:
     std::unique_ptr<rpc::Colors::Stub> stub_;
 };
 
+class FieldLineOptions {
+public:
+    FieldLineOptions(double near)
+        : near_(near) {}
+
+    GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(FieldLineOptions);
+    
+    double near() const {
+        return near_;
+    }
+private:
+    double near_;
+};
+
+bool IsNearCharge(
+    const FieldLineOptions& opts,
+    const Vec2& pt,
+    const std::vector<std::shared_ptr<Charge>>& charges) {
+    for (auto charge : charges) {
+        if (Vec2::sub(charge->point(), pt).length() < opts.near()) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void ComputeFieldLine(
     std::vector<Vec2>* path,
+    const FieldLineOptions& opts,
     const Charge& charge,
     const Vec2& start,
     const std::vector<std::shared_ptr<Charge>>& charges) {
@@ -132,6 +175,10 @@ void ComputeFieldLine(
             dir.scale(7 / dir.length())
         );
         path->push_back(curr);
+
+        if (IsNearCharge(opts, curr, charges)) {
+            return;
+        }
     }
 }
 
@@ -156,8 +203,16 @@ int main(int argc, char* argv[]) {
     double width = 1600.0;
     double height = 600.0;
 
+    std::default_random_engine rng;
+    Seed(rng);
+
     std::vector<std::shared_ptr<Charge>> charges;
-    CreateCharges(&charges, width, height, 20);
+    CreateCharges(
+        &charges,
+        rng,
+        width,
+        height,
+        20);
 
     auto surface = Cairo::ImageSurface::create(
         Cairo::ImageSurface::Format::ARGB32,
@@ -169,42 +224,40 @@ int main(int argc, char* argv[]) {
     context->rectangle(0, 0, width, height);
     context->fill();
 
+    context->save();
     std::vector<Vec2> path;
     path.reserve(1000);
-    auto charge = charges[0];
-    ComputeFieldLine(
-        &path,
-        *charge,
-        Vec2::add(charge->point(), Vec2(1, 0)),
-        charges);
-
-    context->save(); 
-    colors[3].Set(*context);
-    context->begin_new_path();
-    auto it = path.begin();
-    context->move_to(it->i(), it->j());
-    it++;
-    for (; it != path.end(); it++) {
-        context->line_to(it->i(), it->j());
+    FieldLineOptions opts(4.0);
+    context->set_line_width(3.0);
+    colors[3].SetWithAlpha(*context, 0.4);
+    for (auto charge : charges) {
+        auto da = kTau / 64;
+        for (auto a = 0.0; a < kTau; a += da) {
+            Vec2 off(opts.near() * cos(a), opts.near() * sin(a));
+            ComputeFieldLine(
+                &path,
+                opts,
+                *charge,
+                Vec2::add(charge->point(), off),
+                charges);
+            context->begin_new_path();
+            auto it = path.begin();
+            context->move_to(it->i(), it->j());
+            it++;
+            for (; it != path.end(); it++) {
+                context->line_to(it->i(), it->j());
+            }
+            context->stroke();
+        }
     }
-    context->set_line_width(2);
-    context->stroke();
     context->restore();
 
     colors[1].Set(*context);
     for (auto charge : charges) {
         auto pt = charge->point();
         context->begin_new_path();
-        context->arc(pt.i(), pt.j(), 5, 0, 2 * M_PI);
+        context->arc(pt.i(), pt.j(), 2, 0, 2 * M_PI);
         context->fill();
-    }
-
-    colors[2].Set(*context);
-    context->set_line_width(2.0);
-    for (auto charge : charges) {
-        auto pt = charge->point();
-        context->arc(pt.i(), pt.j(), 5, 0, 2 * M_PI);
-        context->stroke();
     }
 
     std::string dst(basename(argv[0]));
